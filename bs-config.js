955e8f2cf72b0eb440e1562b9aa0814cec4b62c3
Gurleen Wadhwa <gurleen.wadhwa@arolytics.com>
2024-05-08 10:06:08 -0300

    Added final changes

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {
     baseDir: "./",
     middleware: {
-      // Middleware to handle history API fallback
+
       1: require("connect-history-api-fallback")({
         index: "/index.html",
       }),
